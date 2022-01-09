c2ed2539034ccaa2be40827a902e69116613d0ef
“Arunesh <as8175802986@gmail.com>
2022-01-09 21:40:38 +0530

    [Arunesh]Added Code for RestSharpAPI

---
+++ RestSharpTestCase/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestSharpTestCase
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public double Salary { get; set; }
+    }
+}
